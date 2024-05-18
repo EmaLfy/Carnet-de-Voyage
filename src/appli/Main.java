24b76b57fc1df99d7955508985eb925e7c4e3f0a
emma <emma.lafay6@etu.univ-lorraine.fr>
2024-05-18 13:58:18 +0200

    TailleComposant

--- src/appli/Main.java
+++ src/appli/Main.java
@@ -1,5 +1,6 @@
 package appli;
 
+import appli.outils.TailleComposant;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,11 +10,12 @@ import javafx.stage.Stage;
 public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         // Charge le fichier FXML
+        TailleComposant tailleComposant = TailleComposant.getInstance();
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/firstPage.fxml"));
 
         // Configure la scène
         primaryStage.setTitle("My JavaFX Application");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(new Scene(root, tailleComposant.windWidth(), tailleComposant.windHeight()));
 
         // Affiche la fenêtre
         primaryStage.show();
---
+++ src/appli/outils/TailleComposant.java
@@ -0,0 +1,22 @@
+package appli.outils;
+
+public class TailleComposant {
+    private static final TailleComposant instance = new TailleComposant();
+
+    private TailleComposant(){
+    }
+
+    public static TailleComposant getInstance() {
+        return instance;
+    }
+
+    public int windHeight(){
+        return 600;
+    }
+    public int windWidth(){
+        return 900;
+    }
+    public int VBoxWidth(){
+        return 200;
+    }
+}
